d38dd70b58cfbc0d3fae7848bbcda8a1b8eb085b
Roma <romamarusyk@gmail.com>
2016-04-19 22:42:27 +0300

    Mapping to SYS_Dictionary

---
+++ SV.Domain/Domain/Mapping/SystemEntities/DictionaryMap.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.Entity.ModelConfiguration;
+using Domain.Entities.SystemEntities;
+
+namespace Domain.Mapping.SystemEntities
+{
+    public class DictionaryMap : EntityTypeConfiguration<SYS_Dictionary>
+    {
+        public DictionaryMap()
+        {
+            HasKey(t => t.ID);
+            Property(t => t.ID).HasDatabaseGeneratedOption(DatabaseGeneratedOption.Identity);
+            Property(t => t.Name).IsRequired().HasMaxLength(50);
+            Property(t => t.Description).IsRequired().HasMaxLength(250);
+            Property(t => t.IsStatic).IsRequired();
+            Property(t => t.LastUpdUS).IsRequired().HasMaxLength(50);
+            ToTable("SYS_Dictionary");
+        }
+    }
+}
