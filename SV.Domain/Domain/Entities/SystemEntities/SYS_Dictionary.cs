8b3184fe25495628aaf93fd42fc879b426b3db64
Roma <romamarusyk@gmail.com>
2016-04-19 22:38:10 +0300

    Added SYS_Dictionary as a system entity

---
+++ SV.Domain/Domain/Entities/SystemEntities/SYS_Dictionary.cs
@@ -0,0 +1,13 @@
+﻿using Domain.Abstract;
+
+namespace Domain.Entities.SystemEntities
+{
+    public class SYS_Dictionary : BaseEntity
+    {
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public bool IsStatic { get; set; }
+    }
+}
