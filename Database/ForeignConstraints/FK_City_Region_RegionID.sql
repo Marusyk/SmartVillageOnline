f203cc6a0d283d43cb396fe66106a1675d4834dd
Roma <romamarusyk@gmail.com>
2016-05-06 02:29:00 +0300

    Added tables to the database project

---
+++ Database/ForeignConstraints/FK_City_Region_RegionID.sql
@@ -0,0 +1,4 @@
+﻿ALTER TABLE [dbo].[City]
+	ADD CONSTRAINT [FK_City_Region_RegionID]
+	FOREIGN KEY ([RegionID])
+	REFERENCES [Region] ([ID])
---
+++ Database/Tables/City.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[City]
+(
+	[Id] INT IDENTITY(1,1) NOT NULL PRIMARY KEY
+,   [Name] [nvarchar](50) NOT NULL
+,   [CityTypeID] [int] NOT NULL
+,   [DistrictID] [int] NULL
+,   [RegionID] [int] NOT NULL
+,   [LastUpdDT] [smalldatetime] NOT NULL
+,   [LastUpdUS] [nvarchar](50) NOT NULL	
+)
