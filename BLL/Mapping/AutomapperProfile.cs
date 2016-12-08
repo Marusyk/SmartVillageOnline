b321de76fb49d404aaf842e030e23f526fde45bb
Roma <romamarusyk@gmail.com>
2016-12-08 22:29:50 +0200

    Add mapping for all entities

--- BLL/Mapping/AutomapperProfile.cs
+++ BLL/Mapping/AutomapperProfile.cs
@@ -12,6 +12,35 @@ namespace BLL.Mapping
 		{
 			CreateMap<Animal, AnimalEntity>();
 			CreateMap<Council, CouncilEntity>();
+			CreateMap<ActivityType, ActivityTypeEntity>();
+			CreateMap<Address, AddressEntity>();
+			CreateMap<City, CityEntity>();
+			CreateMap<CityType, CityTypeEntity>();
+			CreateMap<Company, CompanyEntity>();
+			CreateMap<Country, CountryEntity>();
+			CreateMap<District, DistrictEntity>();
+			CreateMap<DocumentType, DocumentTypeEntity>();
+			CreateMap<EducationDegree, EducationDegreeEntity>();
+			CreateMap<FamilyRelations, FamilyRelationsEntity>();
+			CreateMap<FamilyStatus, FamilyStatusEntity>();
+			CreateMap<Institution, InstitutionEntity>();
+			CreateMap<Material, MaterialEntity>();
+			CreateMap<Nationality, NationalityEntity>();
+			CreateMap<PassAuthority, PassAuthorityEntity>();
+			CreateMap<PensionType, PensionTypeEntity>();
+			CreateMap<Person, PersonEntity>();
+			CreateMap<Position, PositionEntity>();
+			CreateMap<Region, RegionEntity>();
+			CreateMap<Speciality, SpecialityEntity>();
+			CreateMap<Street, StreetEntity>();
+			CreateMap<StreetType, StreetType>();
+			CreateMap<Catalog, CatalogEntity>();
+			CreateMap<Document, DocumentEntity>();
+			CreateMap<Education, EducationEntity>();
+			CreateMap<Employment, EmploymentEntity>();
+			CreateMap<House, HouseEntity>();
+			CreateMap<People, PeopleEntity>();
+			CreateMap<PersonDocument, PersonDocumentEntity>();
 		}
 	}
 }
